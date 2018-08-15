aeebbfa017b78c238a3662fd6a70b4e293ecca13
root <root@DESKTOP-R4DIH9S.localdomain>
2018-08-16 00:13:40 +0800

    feat: README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gulp-minify
 gulp压缩html,css,js,img工作流
+
+usage:
+
+1. clone仓库
+2. 仓库目录下运行npm i
+3. 配置pathConfig.js
+4. 运行npm脚本 npm run min
--- pathConfig.js
+++ pathConfig.js
@@ -1,5 +1,5 @@
-const srcDir = '../mindsee-official-website'
-const distDir = '../dist'
+const srcDir = '../mindsee-official-website' // 要压缩的资源目录
+const distDir = '../dist'  // 压缩后的资源存放目录
 
 module.exports = {
 	srcDir,
